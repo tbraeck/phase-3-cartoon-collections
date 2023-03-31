110245670a8ec724fe09a045af1c256a0d49ea51
Tate <tatebraeckel@gmail.com>
2023-03-31 12:01:01 -0500

    commit tree f5fe7121b3d6a58967f398162f6e406ace9290fb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,49 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+ dwarves.each_with_index do |dwarf, index|
+ puts " #{index + 1}. #{dwarf}"
+
+
+  # puts dwarf.with_index
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+roll_call_dwarves(dwarves)
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+planeteer_calls.map {|call| call.capitalize + '!'}
+  # return [#{call}!]
+end
+ summon_captain_planet(planeteer_calls)
+
+
+#  short_words = ["puff", "go", "two"]
+# assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(planeteer_calls)
+
+planeteer_calls.any? { |call| call.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+long_planeteer_calls(planeteer_calls)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+new_items = ["rugs", "socks", "cream cheese", "cake"]
+
+def find_the_cheese(new_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  new_items.find do |item| 
+cheese_types.include?(item) 
+ 
+  end
 end
+find_the_cheese(new_items)
+
+frogs = ["bull", "jumper"]
+binding.pry
